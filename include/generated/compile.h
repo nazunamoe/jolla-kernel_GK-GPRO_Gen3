c193bd5de29ed594e295fc5a65b2a7a294ce94b5
Will Deacon <will.deacon@arm.com>
2012-08-16 18:14:14 +0100

    tracing/syscalls: Fix perf syscall tracing when syscall_nr == -1
    syscall_get_nr can return -1 in the case that the task is not executing
    a system call.
    
    This patch fixes perf_syscall_{enter,exit} to check that the syscall
    number is valid before using it as an index into a bitmap.
    
    Link: http://lkml.kernel.org/r/1345137254-7377-1-git-send-email-will.deacon@arm.com
    
    Change-Id: Iedc719957e184c6572b3ad94e241ae2a97a0b533
    Cc: Jason Baron <jbaron@redhat.com>
    Cc: Wade Farnsworth <wade_farnsworth@mentor.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    
    Conflicts:
    	kernel/trace/trace_syscalls.c

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 19 */
+/* This file is auto generated, version 20 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#19 SMP PREEMPT Fri Aug 7 19:41:33 PDT 2015"
+#define UTS_VERSION "#20 SMP PREEMPT Sat Aug 15 16:40:04 PDT 2015"
 #define LINUX_COMPILE_BY "justakan"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.9.3 20150311 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.03 - Cortex-A15) "
