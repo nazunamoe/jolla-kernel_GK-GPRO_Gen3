2cc27fb9c0d856abeb72a19691297563c9ad92b2
JustAkan <puppy1900@naver.com>
2015-07-17 19:47:11 -0700

    Jolla kerenl f240,f220k,e980 support Thanks to Jollaman999
    Signed-off-by: JustAkan <puppy1900@naver.com>

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 8 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Sat Jul 11 17:20:57 PDT 2015"
+#define UTS_VERSION "#8 SMP PREEMPT Fri Jul 17 19:42:44 PDT 2015"
 #define LINUX_COMPILE_BY "justakan"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.9.3 20150311 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.03 - Cortex-A15) "
