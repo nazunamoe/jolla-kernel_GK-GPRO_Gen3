fbb3a8fb8f34205feca9230da7710055b491527c
jollaman999 <admin@jollaman999.com>
2015-08-21 22:51:33 +0900

    jolla-kernel_Gen3-v3.5
    Change-Id: I3e1ac5f7a497c61d0d21a6bd4b042ab17a3f9de0
    
    Conflicts:
    	.config
    	Makefile
    	arch/arm/configs/gproj_defconfig

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 20 */
+/* This file is auto generated, version 23 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#20 SMP PREEMPT Sat Aug 15 16:40:04 PDT 2015"
+#define UTS_VERSION "#23 SMP PREEMPT Thu Aug 20 04:15:25 PDT 2015"
 #define LINUX_COMPILE_BY "justakan"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.9.3 20150311 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.03 - Cortex-A15) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.108-jolla-kernel_Gen3-v3.2-GK"
+#define UTS_RELEASE "3.4.108-jolla-kernel_Gen3-v3.2-E980"
