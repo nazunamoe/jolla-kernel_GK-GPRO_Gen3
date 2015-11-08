80d5ced7e4a951ef33f7b97e8b3cdac728c9c625
nazunamoe <jpg3927@gmail.com>
2015-11-09 07:06:34 +0900

    Revert "fix2"
    This reverts commit f1accb1a2959e2f621f88ff38bbe0f02784d7739.

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 46 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#46 SMP PREEMPT Sun Nov 8 20:49:12 KST 2015"
+#define LINUX_COMPILE_BY "nazunamoe"
+#define LINUX_COMPILE_HOST "Hinatabi-Bitter-Sweets"
+#define LINUX_COMPILER "gcc version 5.2.1 20151001 (UBERTC-5.2.x) "
